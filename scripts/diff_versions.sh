2b541947d83f9aee68cbc769e246b829aca66751
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2022-01-11 15:06:04 -0500

    Fix typo in url

--- scripts/diff_versions.sh
+++ scripts/diff_versions.sh
@@ -7,7 +7,7 @@ app=ansible
 existing_tags=$(docker run --rm quay.io/skopeo/stable --override-os linux inspect docker://gableroux/$app | jq '.RepoTags[]' -r  | sort)
 echo "$existing_tags" > versions.existing.txt
 
-PACKAGE_JSON_URL="https://pypi.org/pypi/$app}/json"
+PACKAGE_JSON_URL="https://pypi.org/pypi/$app/json"
 all_tags=$(curl -L -s "$PACKAGE_JSON_URL" | jq  -r '.releases | keys | .[]' | sort)
 echo "$all_tags" > versions.all.txt
 
