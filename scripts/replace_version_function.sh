97efa2f11188e5809e4dbad359f846e6f1ac2d48
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2022-01-11 14:55:47 -0500

    Fix readme sed command

--- scripts/replace_version_function.sh
+++ scripts/replace_version_function.sh
@@ -4,7 +4,7 @@ replace_version() {
     new_version=$1
 
     sed -i "s/ansible==.*/ansible==${new_version}/g" requirements.txt
-    sed -i "s/Ansible==.*/Ansible==${new_version}/g" readme.md
+    sed -i "s/ansible:.*/ansible:${new_version}/g" ReadMe.md
 
     cat requirements.txt
 
